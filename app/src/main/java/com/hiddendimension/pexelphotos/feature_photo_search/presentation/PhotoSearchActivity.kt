dbc3df59795a78b897c5814e41f4f78cb30171c1
alif <alifchoyon@egmail.com>
2022-04-27 21:38:45 +0900

    PhotoSearchActivity created.

---
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.hiddendimension.pexelphotos.feature_photo_search.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hiddendimension.pexelphotos.R
+
+class PhotoSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_search)
+    }
+}
