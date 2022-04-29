085f1e1b107c2a845ea10f72efa1d3a54c33c05e
alif <alifchoyon@egmail.com>
2022-04-29 18:09:18 +0900

    View Model has been updated based on Ui state and Actions.

--- app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchActivity.kt
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchActivity.kt
@@ -2,13 +2,27 @@ package com.hiddendimension.pexelphotos.feature_photo_search.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.hiddendimension.pexelphotos.R
+import com.hiddendimension.pexelphotos.databinding.ActivityPhotoSearchBinding
+import com.hiddendimension.pexelphotos.di.AppModule
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PhotoSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_photo_search)
+
+        val pexelPhotoSearchActivityBinding =
+            ActivityPhotoSearchBinding.inflate(layoutInflater)
+        val pexelPhotoSearchActivityView = pexelPhotoSearchActivityBinding.root
+        setContentView(pexelPhotoSearchActivityView)
+
+        val pexelPhotoSearchActivityViewModel =
+            ViewModelProvider(this, AppModule.provideViewModelFactory(owner = this))
+                .get(PhotoSearchViewModel::class.java)
+
+
+
     }
 }
--- app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchViewModel.kt
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchViewModel.kt
@@ -1,12 +1,83 @@
 package com.hiddendimension.pexelphotos.feature_photo_search.presentation
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.hiddendimension.pexelphotos.feature_photo_search.data.repository.PexelPhotoRepository
+import com.hiddendimension.pexelphotos.feature_photo_search.domain.model.PhotoSearchResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PhotoSearchViewModel @Inject constructor(
     private val repository: PexelPhotoRepository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel()  {
+
+    val state: LiveData<UiState>
+
+    val accept: (SearchUiAction) -> Unit
+
+    init {
+        val queryLiveData =
+            MutableLiveData(savedStateHandle.get(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY)
+
+
+        state = queryLiveData
+            .switchMap { queryString ->
+                liveData {
+                    val uiState = repository.getSearchResultStream(queryString)
+                        .map {
+                            UiState(
+                                query = queryString,
+                                searchResult = it
+                            )
+                        }
+                        .asLiveData(Dispatchers.Main)
+                    emitSource(uiState)
+                }
+            }
+
+
+        accept = { action ->
+            when (action) {
+                is SearchUiAction.Search -> queryLiveData.postValue(action.query)
+                is SearchUiAction.Scroll -> if (action.shouldFetchMore) {
+                    val immutableQuery = queryLiveData.value
+                    if (immutableQuery != null) {
+                        viewModelScope.launch {
+                            repository.requestMore(immutableQuery)
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    override fun onCleared() {
+        savedStateHandle[LAST_SEARCH_QUERY] = state.value?.query
+        super.onCleared()
+    }
 }
+
+private val SearchUiAction.Scroll.shouldFetchMore
+    get() = visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount
+
+
+sealed class SearchUiAction {
+    data class Search(val query: String) : SearchUiAction()
+    data class Scroll(
+        val visibleItemCount: Int,
+        val lastVisibleItemPosition: Int,
+        val totalItemCount: Int
+    ) : SearchUiAction()
+}
+
+data class UiState(
+    val query: String,
+    val searchResult: PhotoSearchResult
+)
+
+private const val VISIBLE_THRESHOLD = 5
+private const val LAST_SEARCH_QUERY: String = "last_search_query"
+private const val DEFAULT_QUERY = "Sun"
