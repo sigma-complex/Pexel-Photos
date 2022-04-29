4e5e7936e54a55db5d9b770985b36d06437348c7
alif <alifchoyon@egmail.com>
2022-04-29 18:23:55 +0900

    List view has has been bound.

--- app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchActivity.kt
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_search/presentation/PhotoSearchActivity.kt
@@ -3,12 +3,17 @@ package com.hiddendimension.pexelphotos.feature_photo_search.presentation
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.map
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hiddendimension.pexelphotos.databinding.ActivityPhotoSearchBinding
 import com.hiddendimension.pexelphotos.di.AppModule
+import com.hiddendimension.pexelphotos.feature_photo_search.domain.model.PhotoSearchResult
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,8 +51,16 @@ class PhotoSearchActivity : AppCompatActivity() {
             onQueryChanged = uiActions
         )
 
+        bindList(
+            repoAdapter = repoAdapter,
+            uiState = uiState,
+            onScrollChanged = uiActions
+        )
+
     }
 
+
+    //binding search state
     private fun ActivityPhotoSearchBinding.bindSearch(
         uiState: LiveData<UiState>,
         onQueryChanged: (SearchUiAction.Search) -> Unit
@@ -82,4 +95,62 @@ class PhotoSearchActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    // list binding
+    private fun ActivityPhotoSearchBinding.bindList(
+        repoAdapter: PhotosAdapter,
+        uiState: LiveData<UiState>,
+        onScrollChanged: (SearchUiAction.Scroll) -> Unit
+    ) {
+        setupScrollListener(onScrollChanged)
+
+        uiState
+            .map(UiState::searchResult)
+            .observe(this@PhotoSearchActivity) { result ->
+                when (result) {
+                    is PhotoSearchResult.Success -> {
+                        showEmptyList(result.data.isEmpty())
+                        repoAdapter.submitList(result.data)
+                        repoAdapter.notifyDataSetChanged()
+                    }
+                    is PhotoSearchResult.Error -> {
+                        Toast.makeText(
+                            this@PhotoSearchActivity,
+                            "\uD83D\uDE28 Wooops $result.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+    }
+
+
+    private fun ActivityPhotoSearchBinding.setupScrollListener(
+        onScrollChanged: (SearchUiAction.Scroll) -> Unit
+    ) {
+        val layoutManager = list.layoutManager as LinearLayoutManager
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val totalItemCount = layoutManager.itemCount
+                val visibleItemCount = layoutManager.childCount
+                val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
+
+                onScrollChanged(
+                    SearchUiAction.Scroll(
+                        visibleItemCount = visibleItemCount,
+                        lastVisibleItemPosition = lastVisibleItem,
+                        totalItemCount = totalItemCount
+                    )
+                )
+            }
+        })
+    }
+
+    private fun ActivityPhotoSearchBinding.showEmptyList(show: Boolean) {
+        emptyList.isVisible = show
+        list.isVisible = !show
+    }
+
 }
