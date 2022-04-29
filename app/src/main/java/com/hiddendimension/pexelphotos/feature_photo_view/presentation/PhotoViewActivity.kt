adaaea72a129bff694d5923c6626a972eb293ed5
alif <alifchoyon@egmail.com>
2022-04-29 18:43:32 +0900

    Full screen PhotoViewActivity now shows the image in full screen.

--- app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_view/presentation/PhotoViewActivity.kt
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_view/presentation/PhotoViewActivity.kt
@@ -2,11 +2,40 @@ package com.hiddendimension.pexelphotos.feature_photo_view.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.hiddendimension.pexelphotos.R
+import com.hiddendimension.pexelphotos.databinding.ActivityPhotoViewBinding
 
 class PhotoViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_photo_view)
+
+        val fullScreenImageViewActivityBinding =
+            ActivityPhotoViewBinding.inflate(layoutInflater)
+        val fullScreenImageViewActivityView = fullScreenImageViewActivityBinding.root
+        setContentView(fullScreenImageViewActivityView)
+
+
+        ViewModelProvider(this)
+            .get(FullScreenImageViewActivityViewModel::class.java)
+
+        val url = intent.getStringExtra("url")
+
+        if (url != null) {
+            loadImage(url, fullScreenImageViewActivityBinding.imageView)
+        }
+
+
+    }
+
+    private fun loadImage(url: String, view: ImageView) {
+        Glide.with(view.context)
+            .load(url)
+            .transform(CenterInside(), RoundedCorners(24))
+            .into(view)
     }
 }
---
+++ app/src/main/java/com/hiddendimension/pexelphotos/feature_photo_view/presentation/PhotoViewActivityViewModel.kt
@@ -0,0 +1,5 @@
+package com.hiddendimension.pexelphotos.feature_photo_view.presentation
+
+import androidx.lifecycle.ViewModel
+
+class FullScreenImageViewActivityViewModel : ViewModel()
